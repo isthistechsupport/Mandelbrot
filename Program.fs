b7871625457cdd9b67c4abcd66b598cbe461a5a1
Diego Rivero <32936232+isthistechsupport@users.noreply.github.com>
2024-05-06 12:04:57 -0500

    added hsl colors and new benchmarking

--- Program.fs
+++ Program.fs
@@ -8,6 +8,9 @@ open SixLabors.ImageSharp
 open SixLabors.ImageSharp.PixelFormats
 open System.Diagnostics
 open System.Threading.Tasks
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open BenchmarkDotNet.Jobs
 
 
 type CliArguments =
@@ -33,16 +36,36 @@ type CliArguments =
 let scale (x0:float) x1 y1 = (x0/x1) * y1
 
 
-let rec coordToMSetValue = function
-| x:float, y:float, x0, y0, i, maxDepth when x*x + y*y <= 4 && i <= maxDepth -> coordToMSetValue (x*x - y*y + x0, 2.0*x*y + y0, x0, y0, i+1, maxDepth)
-| _, _, _, _, i, maxDepth when i >= maxDepth -> 256
-| _, _, _, _, i, maxDepth -> int <| System.Math.Round (scale (float i) (float maxDepth) 255.0)
+let coordToMandelbrotDepth (x: float, y: float, x0, y0, i, maxDepth) =
+    let rec loop (x: float, y: float, i) =
+        let x2, y2 = x*x, y*y
+        match x2 + y2 <= 4.0 && i < maxDepth with
+        | true -> loop (x2 - y2 + x0, 2.0*x*y + y0, i + 1)
+        | false -> i, maxDepth
+    loop (x, y, i)
 
 
-let mSetValueToPixelTuple = function
-| x when x = 256 -> (255uy, 255uy, 255uy)
-| x when x > 127 -> (byte x, 255uy, byte x)
-| x -> (0uy, byte x, 0uy)
+let mandelbrotDepthToLightness (depth,  maxDepth) = scale (float depth) (float maxDepth) 1.0
+
+
+let lightnessToPixelTuple = function
+| lightness when lightness = 0.0 || lightness = 1.0 -> (0uy, 0uy, 0uy)
+| lightness ->
+    let hue = 210.0
+    let saturation = 1.0
+    let c = (1.0 - abs(2.0 * lightness - 1.0)) * saturation
+    let x = c * (1.0 - abs((hue / 60.0) % 2.0 - 1.0))
+    let m = lightness - c / 2.0
+    let r', g', b' = 
+        match int hue / 60 with
+        | 0 -> c, x, 0.0
+        | 1 -> x, c, 0.0
+        | 2 -> 0.0, c, x
+        | 3 -> 0.0, x, c
+        | 4 -> x, 0.0, c
+        | _ -> c, 0.0, x
+    let r, g, b = r' + m, g' + m, b' + m
+    byte <| System.Math.Round(r * 255.0), byte <| System.Math.Round(g * 255.0), byte <| System.Math.Round(b * 255.0)
 
 
 let pixelTupleToPixel (r, g, b) = new Rgb24(r, g, b)
@@ -51,16 +74,13 @@ let pixelTupleToPixel (r, g, b) = new Rgb24(r, g, b)
 let nToWrittenPixel n imageSideLen windowSideLen x0 y0 maxDepth (image : Image<Rgb24>) =
     let x = n % imageSideLen
     let y = n / imageSideLen
-    let pixel = (0.0, 0.0, ((scale (float x) (float imageSideLen) (windowSideLen)) + (x0)), ((scale (float y) (float imageSideLen) (windowSideLen)) + (y0)), 0, maxDepth) |> (coordToMSetValue >> mSetValueToPixelTuple >> pixelTupleToPixel)
+    let pixel = (0.0, 0.0, ((scale (float x) (float imageSideLen) (windowSideLen)) + (x0)), ((scale (float y) (float imageSideLen) (windowSideLen)) + (y0)), 0, maxDepth) |> (coordToMandelbrotDepth >> mandelbrotDepthToLightness >> lightnessToPixelTuple >> pixelTupleToPixel)
     image[x, y] <- pixel
 
 
 let nToWrittenPixelAsync n imageSideLen windowSideLen x0 y0 maxDepth (image : Image<Rgb24>) =
     task {
-        let x = n % imageSideLen
-        let y = n / imageSideLen
-        let pixel = (0.0, 0.0, ((scale (float x) (float imageSideLen) (windowSideLen)) + (x0)), ((scale (float y) (float imageSideLen) (windowSideLen)) + (y0)), 0, maxDepth) |> (coordToMSetValue >> mSetValueToPixelTuple >> pixelTupleToPixel)
-        image[x, y] <- pixel
+        nToWrittenPixel n imageSideLen windowSideLen x0 y0 maxDepth image
     }
 
 
@@ -79,7 +99,37 @@ let dimsToMSetImage imageSideLen windowSideLen x0 y0 maxDepth async =
             image.SaveAsJpeg fileStream))
 
 
-[<EntryPoint>]
+[<SimpleJob(RuntimeMoniker.Net80)>]
+type Benchmarks() =
+    [<Params(128, 256, 512, 1024, 2048, 4096)>]
+    member val ImageSideLength = 0 with get, set
+    
+    [<Benchmark(Baseline = true)>]
+    member this.RenderImage() =
+        let imageSideLen = this.ImageSideLength
+        let windowSideLen = 3.0
+        let x = -0.5
+        let y = 0.0
+        let maxDepth = 50
+        let async = false
+        let x0 = x - (windowSideLen / 2.0)
+        let y0 = y - (windowSideLen / 2.0)
+        dimsToMSetImage imageSideLen windowSideLen x0 y0 maxDepth async
+
+    [<Benchmark>]
+    member this.RenderImageAsync() =
+        let imageSideLen = this.ImageSideLength
+        let windowSideLen = 3.0
+        let x = -0.5
+        let y = 0.0
+        let maxDepth = 50
+        let async = true
+        let x0 = x - (windowSideLen / 2.0)
+        let y0 = y - (windowSideLen / 2.0)
+        dimsToMSetImage imageSideLen windowSideLen x0 y0 maxDepth async
+
+
+//[<EntryPoint>]
 let main argv =
     let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some System.ConsoleColor.Red)
     let parser = ArgumentParser.Create<CliArguments>(errorHandler = errorHandler)
@@ -101,4 +151,10 @@ let main argv =
     let time = stopwatch.Elapsed.TotalMilliseconds
     if not silent then
         printfn "Image rendered in %.2f ms." time
+    0
+
+
+[<EntryPoint>]
+let benchmarker argv =
+    BenchmarkSwitcher.FromAssembly(typeof<Benchmarks>.Assembly).Run(argv) |> ignore
     0
